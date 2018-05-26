e96294b958a62ab2033c336e79af19fafdd80fbf
Patryk Szeliga <szeliga_patryk92@gmail.com>
2018-05-26 20:39:32 +0200

    Temporarily turn off authentication

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -7,7 +7,7 @@ module Api
       respond_to :json
 
       before_action :configure_permitted_parameters, if: :devise_controller?
-      before_action :authenticate_breeder!
+      # before_action :authenticate_breeder!
 
       protected
 
