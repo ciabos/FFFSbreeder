78bfd6539b232ebc844d86da7b6c594214bae290
Patryk Szeliga <szeliga_patryk92@gmail.com>
2018-05-26 21:04:50 +0200

    Add seeds

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem "devise_token_auth"
 gem "omniauth"
 gem "rspec-rails"
 gem "factory_bot_rails", "~> 4.0"
+gem "faker", :git => "https://github.com/stympy/faker.git", :branch => "master"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |n|
+  breeder = Breeder.create( provider: "email",
+    uid: "mail#{n}@example.com",
+    email: "mail#{n}@example.com",
+    password: "password",
+    name: "#{Faker::GameOfThrones.character} #{n}",
+    rattery_name: Faker::GameOfThrones.house
+  )
+  father = Animal.create(
+    sex: "male",
+    name: Faker::HarryPotter.character,
+    home_name: Faker::HarryPotter.house,
+    phenotype: "something",
+    genotype: "something"
+  )
+  mother = Animal.create(
+    sex: "female",
+    name: Faker::HarryPotter.character,
+    home_name: Faker::HarryPotter.house,
+    phenotype: "something",
+    genotype: "something"
+  )
+  litter = Litter.create(
+    name: "#{Faker::LordOfTheRings.character} #{n}",
+    birth_date: Faker::Date.between(2.years.ago, Date.today),
+    expected_traits: ["tall", "thin"],
+    breeder_id: breeder.id,
+    father: father,
+    mother: mother
+  )
+  Animal.create(
+    sex: "male",
+    name: Faker::HarryPotter.character,
+    home_name: Faker::HarryPotter.house,
+    phenotype: "something",
+    genotype: "something",
+    litter: litter
+  )
+  Animal.create(
+    sex: "male",
+    name: Faker::HarryPotter.character,
+    home_name: Faker::HarryPotter.house,
+    phenotype: "something",
+    genotype: "something",
+    litter: litter
+  )
+  Animal.create(
+    sex: "female",
+    name: Faker::HarryPotter.character,
+    home_name: Faker::HarryPotter.house,
+    phenotype: "something",
+    genotype: "something",
+    litter: litter
+  )
+  Animal.create(
+    sex: "female",
+    name: Faker::HarryPotter.character,
+    home_name: Faker::HarryPotter.house,
+    phenotype: "something",
+    genotype: "something",
+    litter: litter
+  )
+end
